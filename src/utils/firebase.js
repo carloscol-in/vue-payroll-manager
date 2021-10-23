8f92d8243bb44d6f9f5e9d79fdc08e8df73067cf
Carlos <contact@carloscolin.me>
2021-10-22 21:44:23 -0500

    added firebase utils

---
+++ src/utils/firebase.js
@@ -0,0 +1,34 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Utils
+const auth = getAuth();
+console.log(auth, typeof (auth));
+
+// exports
+export {
+  auth,
+  signOut,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+};
