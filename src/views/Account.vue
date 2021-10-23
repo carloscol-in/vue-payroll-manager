8bb6fc79444fc821c32dc317f5fbedcd201829d7
Carlos <contact@carloscolin.me>
2021-10-23 00:54:51 -0500

    initial account component commit

---
+++ src/views/Account.vue
@@ -0,0 +1,63 @@
+<template>
+  <BasicLayout>
+    <h1>Welcome to Account Settings</h1>
+
+    <div class="account__section">
+      <p class="title">
+        <span v-if="!user.displayName">Add name</span>
+        <span v-if="user.displayName">Change name ({{ user.displayName }}...)</span>
+      </p>
+      <ChangeName/>
+    </div>
+  </BasicLayout>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import BasicLayout from '../layouts/BasicLayout.vue';
+import ChangeName from '../components/Account/ChangeName.vue';
+
+export default {
+  name: 'Account',
+  components: {
+    BasicLayout,
+    ChangeName,
+  },
+  setup() {
+    const store = useStore();
+    const user = computed(() => store.state.user);
+
+    console.log(user);
+
+    return {
+      user,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.account {
+  h1 {
+    margin: 50px 0;
+    text-align: center;
+  }
+
+  &__section {
+    position: relative;
+    padding: 10px;
+    border: 1px solid #16212b;
+    padding-top: 25px;
+    margin-top: 40px;
+
+    .title {
+      position: absolute;
+      top: -12px;
+      background-color: #16212b;
+      color: #fff;
+      padding: 2px 10px;
+    }
+  }
+}
+</style>
