607a9e96cd251eef2ea3438847e6645235b246c1
Carlos <contact@carloscolin.me>
2021-10-22 21:45:08 -0500

    home initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>at home</h1>
+    <button @click="logout">Sign Out</button>
+  </div>
+</template>
+
+<script>
+import { auth } from '../utils/firebase';
+
+export default {
+  name: 'Home',
+  setup() {
+    const logout = () => {
+      auth.signOut();
+    };
+
+    return {
+      logout,
+    };
+  },
+};
+</script>
