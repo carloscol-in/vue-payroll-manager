2fb4a740feebb97d38eca6fb109c7a42559ee23e
Carlos <contact@carloscolin.me>
2021-10-23 00:53:53 -0500

    added account related components

---
+++ src/components/Account/ChangeName.vue
@@ -0,0 +1,27 @@
+<template>
+  <form action="" class="ui form change-name">
+    <input type="text" placeholder="Name and Surname">
+    <button type="submit" class="ui button primary">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ChangeName',
+};
+</script>
+
+<style lang="scss" scoped>
+.ui.form.change-name {
+  text-align: center;
+
+  input.error {
+    border-color: #faa;
+    background-color: #ffeded;
+  }
+
+  .ui.button {
+    margin: 20px 0 0;
+  }
+}
+</style>
