e4ec891f26e6a038b633c74836c1ff0984234047
Carlos <contact@carloscolin.me>
2021-10-23 00:55:20 -0500

    payrolls component initial commit

---
+++ src/views/Payrolls.vue
@@ -0,0 +1,20 @@
+<template>
+  <BasicLayout>
+    <h1>We're on Payrolls</h1>
+    <p>Hello, world</p>
+  </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from '../layouts/BasicLayout.vue';
+
+export default {
+  components: {
+    BasicLayout,
+  },
+};
+</script>
+
+<style>
+
+</style>
