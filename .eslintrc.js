5cbdd90b2696ac180bd4b106cb6c58ed3bc55d72
Carlos <contact@carloscolin.me>
2021-10-22 21:40:14 -0500

    eslint rules init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'vue/no-unused-components': 'warn',
+    'max-len': 'off',
+  },
+};
