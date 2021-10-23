c5885c065a3649e40cd578f2a368b769fa861dc9
Carlos <contact@carloscolin.me>
2021-10-22 21:44:46 -0500

    added auth views; login and signup

---
+++ src/views/Auth.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="auth">
+    <Login v-if="showLogin" :changeForm="changeForm" />
+    <Signup v-if="!showLogin" :changeForm="changeForm" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { Login, Signup } from '../components/Auth/index';
+
+export default {
+  name: 'Auth',
+  components: {
+    Login, Signup,
+  },
+  setup() {
+    const showLogin = ref(true);
+
+    const changeForm = () => {
+      showLogin.value = !showLogin.value;
+    };
+
+    return {
+      showLogin,
+      changeForm,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.auth {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: rgb(4, 78, 157);
+  background: linear-gradient(
+    0deg,
+  rgba(4, 78, 157, 1) 0%,
+  rgba(0, 174, 255, 1) 100%,
+  )
+}
+</style>
