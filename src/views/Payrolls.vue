473f9c3255a5d8d6fbd449fb984f09f023acf5f0
Carlos <contact@carloscolin.me>
2021-10-27 00:45:11 -0500

    add upload payroll component

--- src/views/Payrolls.vue
+++ src/views/Payrolls.vue
@@ -1,20 +1,27 @@
 <template>
   <BasicLayout>
-    <h1>We're on Payrolls</h1>
-    <p>Hello, world</p>
+    <div class="payrolls">
+      <h1>My Payrolls</h1>
+      <UploadPayroll/>
+    </div>
   </BasicLayout>
 </template>
 
 <script>
 import BasicLayout from '../layouts/BasicLayout.vue';
+import UploadPayroll from '../components/Payrolls/UploadPayroll.vue';
 
 export default {
   components: {
     BasicLayout,
+    UploadPayroll,
   },
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.payrolls {
+  margin: 50px 0;
+  text-align: center;
+}
 </style>
