9cbba37f44da32c87d984eeadf0bfc4fb59b799e
Carlos <contact@carloscolin.me>
2021-10-27 00:44:47 -0500

    add method to update password

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,6 +9,7 @@ import {
   reauthenticateWithCredential,
   EmailAuthProvider,
   updateEmail,
+  updatePassword,
 } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -41,4 +42,5 @@ export {
   reauthenticateWithCredential,
   EmailAuthProvider,
   updateEmail,
+  updatePassword,
 };
