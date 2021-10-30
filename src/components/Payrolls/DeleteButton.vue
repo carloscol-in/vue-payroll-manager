af6502628b47d19678c3b99a8e1a89d81023ada1
Carlos <contact@carloscolin.me>
2021-10-30 00:25:28 -0500

    added delete button to encapsulate loading state

---
+++ src/components/Payrolls/DeleteButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="ui button red" @click="deletePayrollDoc(payroll.id)" :class="{ loading }">Delete</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'DeleteButton',
+  props: {
+    payrollDeleteLoading: Boolean,
+    deletePayroll: Function,
+    payroll: Object,
+  },
+  setup(props) {
+    let loading = ref(false);
+
+    const deletePayrollDoc = async (id) => {
+      loading.value = true;
+
+      await props.deletePayroll(id);
+
+      loading.value = false;
+    };
+
+    return {
+      deletePayrollDoc,
+      loading,
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
